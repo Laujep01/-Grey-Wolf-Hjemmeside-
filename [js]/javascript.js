20c30fe7d1b27a570a00420be4f37750fb44bdcb
Laujep01 <laujep01@iba.dk>
2024-09-30 12:20:49 +0200

    grey wolf update
    grey wolf update

--- [js]/javascript.js
+++ [js]/javascript.js
@@ -6,64 +6,127 @@
 
 fetch_header(); */
 
+/* header */
 
-  async function header() {
+async function header() {
+  let promise = new Promise((resolve, reject) => {
+    fetch("header.html")
+      .then((response) => response.text())
+      .then((data) => {
+        const header = document.getElementById("header");
 
-    let promise = new Promise((resolve, reject) => {
+        const nav = document.createElement("nav");
+        nav.innerHTML = data;
+ 
+        header.appendChild(nav);
 
-        fetch("header.html")
-        .then(response => response.text())
-        .then(data => {
+        console.log(nav);
 
-            const header = document.getElementById("header");
+        const søgefelt = document.getElementById("søgefelt");
+        søgefelt.style.display = "none";
 
-            const nav = document.createElement("nav");
-            nav.innerHTML = data;
+        const burgermenu = document.getElementById("burgermenu"); 
 
-            header.appendChild(nav);
+        const burgermenu_X = document.getElementById("burgermenu_X"); /* skal pt ikke bruges til noget */ 
+        burgermenu_X.style.display = "none";
 
-            console.log(nav);
+        const navigation = document.getElementById("navigation");
+        navigation.style.display = "flex";
+
+        const dropdown_menu = document.getElementById("dropdown_menu");
+        dropdown_menu.style.display = "none";
+       
+
+        const testbox_51 = document.getElementById("testbox_51");
+        testbox_51.style.display = "block";
+
         
+
+        header.addEventListener("click", function(e){
+          let target = e.target.closest("#søgning"); // target has id "søgning"
         
+          if(target){ // do something when clicking element with id "søgning" (toggle visibility of "søgefelt" and "testbox_51")
+            if (søgefelt.style.display === "none") {
+              søgefelt.style.display = "block";
+              testbox_51.style.display = "none";
+              søgefelt.focus();
+            } else {
+              testbox_51.style.display = "block";
+              søgefelt.style.display = "none";
+            }
+
+         }
+ 
+
+
+         target = e.target.closest("#burgermenu"); // target has id "burgermenu"
         
-        }) 
-
-    });
-  
-    let result = await promise; // wait until the promise resolves (*)
-  
-    alert(result); // "done!"
-  }
-  
-  header();
+         if(target){ // do something when clicking element with id "burgermenu" (toggle visibility of "navigation" and "dropdown_menu")
+       
+          navigation.style.display = "none";
+          dropdown_menu.style.display = "block";
+           
+        }
 
 
-  async function footer() {
 
-    let promise = new Promise((resolve, reject) => {
 
-        fetch("footer.html")
-        .then(response => response.text())
-        .then(data => {
-
-            const footer = document.getElementById("footer");
-
-            const section = document.createElement("section");
-            section.innerHTML = data;
-
-            footer.appendChild(section);
-
-            console.log(section);
+        target = e.target.closest("#dropdown_menu"); // target has id "burgermenu"
         
-        
-        
-        }) 
+        if(target){ // do something when clicking element with id "burgermenu" (toggle visibility of "navigation" and "dropdown_menu")
+      
+         dropdown_menu.style.display = "none";
+         navigation.style.display = "flex";
+          
+       }
 
-    });
-  
-    let result = await promise; // wait until the promise resolves (*)
-  
-    alert(result); // "done!"
-  }
-  
-  footer();
+
+
+          
+        });
+
+      });
+  });
+
+  let result = await promise; // wait until the promise resolves (*)
+
+  alert(result); // "done!"
+}
+
+header();
+
+/* footer */
+
+async function footer() {
+  let promise = new Promise((resolve, reject) => {
+    fetch("footer.html")
+      .then((response) => response.text())
+      .then((data) => {
+        const footer = document.getElementById("footer");
+
+        const section = document.createElement("section");
+        section.innerHTML = data;
+
+        footer.appendChild(section);
+
+        console.log(section);
+      });
+  });
+
+  let result = await promise; // wait until the promise resolves (*)
+
+  alert(result); // "done!"
+}
+
+footer();
+
+ 
+
+ 
+
+/* 
+addEventListener("DOMContentLoaded", (event) => {
+  const søgefelt = document.getElementById("søgefelt");
+  søgefelt.style.display = "none";
+}); 
+*/
